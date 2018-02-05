1d2d0549eb2026d5935a5370ba7f313ded0d7013
Jamie Briggs <jamie.briggs@hotmail.co.uk>
2018-02-05 14:06:36 +0000

    Added Combined Games Project to Git
    Created project and added project to Git

---
+++ Source/CombinedGroupProject/CombinedGroupProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "CombinedGroupProjectGameMode.h"
+#include "CombinedGroupProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACombinedGroupProjectGameMode::ACombinedGroupProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/CombinedGroupProject/CombinedGroupProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombinedGroupProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACombinedGroupProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACombinedGroupProjectGameMode();
+};
+
+
+
