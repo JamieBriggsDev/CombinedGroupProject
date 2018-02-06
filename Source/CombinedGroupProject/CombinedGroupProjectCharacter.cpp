32e87b86471af24325e4fbf8747ca917888771a9
hawkeyelyons <36172952+hawkeyelyons@users.noreply.github.com>
2018-02-06 12:23:05 +0000

    Added AS-L to project
    //

--- Source/CombinedGroupProject/CombinedGroupProjectCharacter.cpp
+++ Source/CombinedGroupProject/CombinedGroupProjectCharacter.cpp
@@ -10,7 +10,7 @@
 #include "GameFramework/SpringArmComponent.h"
 
 //////////////////////////////////////////////////////////////////////////
-// ACombinedGroupProjectCharacter
+//// ACombinedGroupProjectCharacter
 
 ACombinedGroupProjectCharacter::ACombinedGroupProjectCharacter()
 {
